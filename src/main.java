04772d215467fe7f3811cd3fbe3983fee2ae3ee7
Ader-EX <musyaffa262@gmail.com>
2022-11-09 12:00:31 +0700

    TEST COMMIT DUA

--- src/main.java
+++ src/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) throws SQLException {
         home panelHome = new home();
         // TESTING COMMIT
+        // TESTING COMMIT DUA
     }
 }
