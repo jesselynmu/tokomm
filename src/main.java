09dcd537d6d7279b41708bf06ce4fbedb853743f
Ader-EX <musyaffa262@gmail.com>
2022-11-09 11:58:12 +0700

    AAAAA

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.sql.SQLException;
 
 public class main {
     public static void main(String[] args) throws SQLException {
-        //test komen
         home panelHome = new home();
+        // TESTING COMMIT
     }
 }
