4fdc56f29e7792f1738be16ff7b043a5734c8ed5
jesselynmu <mujesselyn@gmail.com>
2022-11-16 11:23:55 +0700

    testing testing

---
+++ src/transaksiPage.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class transaksiPage extends JFrame{
+    private JTextField textField1;
+    private JTextField textField2;
+    private JSpinner spinner1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JButton button1;
+    private JTable table1;
+    private JTextArea textArea1;
+    private JPanel transaksiPage;
+
+    public transaksiPage() {
+        setContentPane(transaksiPage);
+        setSize(1280, 720);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
