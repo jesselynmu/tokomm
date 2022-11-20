d129a975a41269266193104a287e542c27639d43
jesselynmu <mujesselyn@gmail.com>
2022-11-20 20:45:53 +0700

    testing testing testing

---
+++ src/LogTransaksi.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class LogTransaksi {
+    private JTable table1;
+    private JButton backButton;
+}
---
+++ src/detailKaryawan.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class detailKaryawan {
+    private JPanel panel1;
+    private JTextField tfQuantity;
+    private JTextField tfHarga;
+    private JButton addButton;
+    private JTextField tfNama;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JTable table1;
+    private JButton backButton;
+}
